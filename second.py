fd2c71c9f66c79e5bfc36b1d372af35f6484f3cc
Deepakrocknow <deepaktomar3122003@gmail.com>
2024-01-19 01:11:18 +0530

    all

---
+++ second.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+gameWindow = pygame.display.set_mode((1200,500))
+pygame.display.set_caption("my game")
+
+exit_game = False
+game_over = False
+
+while not exit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                print("CLICKED RIGHT")
+
+
+pygame.quit()
+quit()
